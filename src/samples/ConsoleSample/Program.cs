89af14ecaaa914408f0f1889454e58964b79699d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-03-02 20:27:05 +0100

    Create sample view models

--- src/samples/ConsoleSample/Program.cs
+++ src/samples/ConsoleSample/Program.cs
@@ -3,9 +3,100 @@ using Jab;
 
 namespace ConsoleSample
 {
+    public abstract partial class BaseViewModel
+    {
+    }
+
+    public partial class OptimisePrivacyViewModel : BaseViewModel
+    {
+        private IServiceProvider _serviceProvider;
+        
+        public OptimisePrivacyViewModel(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+    }
+    
+    public partial class PrivacyControlViewModel : BaseViewModel
+    {
+        private IServiceProvider _serviceProvider;
+        
+        public PrivacyControlViewModel(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+    }
+    
+    public partial class PrivacySuggestionControlViewModel : BaseViewModel
+    {
+        private IServiceProvider _serviceProvider;
+        
+        public PrivacySuggestionControlViewModel(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+    }
+
+    public partial class SendViewModel : BaseViewModel
+    {
+        private IServiceProvider _serviceProvider;
+        
+        public SendViewModel(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            Console.WriteLine("SendViewModel()");
+        }
+    }
+
+    public partial class TransactionPreviewViewModel : BaseViewModel
+    {
+        private IServiceProvider _serviceProvider;
+        
+        public TransactionPreviewViewModel(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+    }
+
+    [ServiceProviderModule]
+    [Transient(typeof(OptimisePrivacyViewModel))]
+    [Transient(typeof(PrivacyControlViewModel))]
+    [Transient(typeof(PrivacySuggestionControlViewModel))]
+    [Transient(typeof(SendViewModel))]
+    [Transient(typeof(TransactionPreviewViewModel))]
+    public interface IWalletSendModule
+    {
+    }
+
+    public partial class ReceiveViewModel : BaseViewModel
+    {
+        private IServiceProvider _serviceProvider;
+        
+        public ReceiveViewModel(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            Console.WriteLine("ReceiveViewModel()");
+        }
+    }
+
+    public partial class AddWalletViewModel : BaseViewModel
+    {
+        private IServiceProvider _serviceProvider;
+        
+        public AddWalletViewModel(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            Console.WriteLine("AddWalletViewModel()");
+        }
+    }
+
     [ServiceProvider]
+    [Singleton(typeof(IServiceProvider), typeof(ServiceProvider))]
     [Singleton(typeof(Program))]
     [Singleton(typeof(Logger))]
+    [Scoped(typeof(ReceiveViewModel))]
+    [Singleton(typeof(AddWalletViewModel))]
+    [Import(typeof(IWalletSendModule))]
     partial class ServiceProvider
     {
     }
@@ -32,7 +123,43 @@ namespace ConsoleSample
             _logger = logger;
         }
 
-        static void Main(string[] args) => new ServiceProvider().GetService<Program>().Run(args);
+        static void Main(string[] args)
+        {
+            var service = new ServiceProvider();
+
+            IServiceProvider serviceProvider = service;
+
+            using (var scope1 = service.CreateScope())
+            {
+                var sendViewModel = scope1.GetService<SendViewModel>();
+                var receiveViewModel = scope1.GetService<ReceiveViewModel>();
+                var addWalletViewModel = scope1.GetService<AddWalletViewModel>();
+            }
+
+            using (var scope2 = service.CreateScope())
+            {
+                var sendViewModel = scope2.GetService<SendViewModel>();
+                var receiveViewModel = scope2.GetService<ReceiveViewModel>();
+                var addWalletViewModel = scope2.GetService<AddWalletViewModel>();
+            }
+
+            var rootScope = service.CreateScope();
+            var logger1 = rootScope.GetService<Logger>();
+            var logger2 = service.GetService<Logger>();
+            var logger3 = serviceProvider.GetService(typeof(Logger));
+            
+            Console.WriteLine(logger1 == logger2);
+            Console.WriteLine(logger2 == logger3);
+
+            var receiveViewModel1 = service.GetService<ReceiveViewModel>();
+            var receiveViewModel2 = service.GetService<ReceiveViewModel>();
+            Console.WriteLine(receiveViewModel1 == receiveViewModel2);
+            var receiveViewModel3 = rootScope.GetService<ReceiveViewModel>();
+            
+            var addWalletViewModel1 = service.GetService<AddWalletViewModel>();
+
+            service.GetService<Program>().Run(args);
+        }
 
         public void Run(string[] args)
         {
